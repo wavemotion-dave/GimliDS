793e2b297ede8c66093132530c12a26cf31bad55
Dave Bernazzani <meeplemaster@gmail.com>
2025-05-09 07:54:13 -0400

    First pass at adding REU emulation (just 256K for now)

---
+++ arm9/source/REU.cpp
@@ -0,0 +1,315 @@
+// =====================================================================================
+// GimliDS Copyright (c) 2025 Dave Bernazzani (wavemotion-dave)
+//
+// As GimliDS is a port of the Frodo emulator for the DS/DSi/XL/LL handhelds,
+// any copying or distribution of this emulator, its source code and associated
+// readme files, with or without modification, are permitted per the original 
+// Frodo emulator license shown below.  Hugest thanks to Christian Bauer for his
+// efforts to provide a clean open-source emulation base for the C64.
+//
+// Numerous hacks and 'unsafe' optimizations have been performed on the original 
+// Frodo emulator codebase to get it running on the small handheld system. You 
+// are strongly encouraged to seek out the official Frodo sources if you're at
+// all interested in this emulator code.
+//
+// The GimliDS emulator is offered as-is, without any warranty. Please see readme.md
+// =====================================================================================
+
+/*
+ *  REU.cpp - 17xx REU emulation
+ *
+ *  Frodo Copyright (C) Christian Bauer
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
+ * Incompatibilities:
+ * ------------------
+ *
+ *  - REU interrupts are not emulated.
+ *  - Transfer time is not accounted for, all transfers are done in 0 cycles.
+ */
+
+#include "sysdeps.h"
+
+#include "REU.h"
+#include "CPUC64.h"
+#include "Prefs.h"
+
+extern u8 myRAM[];
+
+/*
+ *  REU constructor
+ */
+
+REU::REU(MOS6510 * cpu) : the_cpu(cpu)
+{
+    // Allocate expansion RAM
+    ram_size = 0x40000; // 256K only
+    ram_mask = ram_size - 1;
+    
+    ex_ram = new uint8_t[ram_size];
+
+    // Clear expansion RAM
+    memset(ex_ram, 0, ram_size);
+
+    // Reset registers
+    Reset();
+}
+
+
+/*
+ *  REU destructor
+ */
+
+REU::~REU()
+{
+    // Free expansion RAM
+    delete[] ex_ram;
+}
+
+
+/*
+ *  Reset the REU
+ */
+
+void REU::Reset()
+{
+    // Set size bit in status register
+    if (ram_size > 0x20000) 
+    {
+        regs[0] = 0x10;
+    }
+    else 
+    {
+        regs[0] = 0x00;
+    }
+
+    // FF00 disabled in command register
+    regs[1] = 0x10;
+
+    for (unsigned i = 2; i < 10; ++i) {
+        regs[i] = 0;
+    }
+
+    // Transfer length = $ffff
+    regs[7] = regs[8] = 0xff;
+
+    // Unconnected registers
+    for (unsigned i = 11; i < 16; ++i) {
+        regs[i] = 0xff;
+    }
+
+    // Autoload registers
+    autoload_c64_adr_lo = 0;
+    autoload_c64_adr_hi = 0;
+    autoload_reu_adr_lo = 0;
+    autoload_reu_adr_hi = 0;
+    autoload_reu_adr_bank = 0;
+    autoload_length_lo = 0xff;
+    autoload_length_hi = 0xff;
+}
+
+
+/*
+ *  Read from REU register
+ */
+
+uint8_t REU::ReadIO2(uint16_t adr, uint8_t bus_byte)
+{
+    if (ex_ram == nullptr)
+        return bus_byte;
+    if ((adr & 0x1f) >= 0x10)
+        return 0xff;
+
+    switch (adr & 0xf) 
+    {
+        case 0:
+        {
+            uint8_t ret = regs[0];
+            regs[0] &= 0x1f;    // Clear status bits
+            return ret;
+        }
+        case 6:
+            return regs[6] | 0xf8;
+        case 9:
+            return regs[9] | 0x1f;
+        case 10:
+            return regs[10] | 0x3f;
+        default:
+            return regs[adr & 0xf];
+    }
+}
+
+
+/*
+ *  Write to REU register
+ */
+
+void REU::WriteIO2(uint16_t adr, uint8_t byte)
+{
+    if (ex_ram == nullptr)
+        return;
+    if ((adr & 0x1f) >= 0x10)
+        return;
+
+    switch (adr & 0xf) 
+    {
+        case 0:     // Status register is read-only
+        case 2:
+            regs[2] = autoload_c64_adr_lo = byte;
+            regs[3] = autoload_c64_adr_hi;
+            break;
+        case 3:
+            regs[2] = autoload_c64_adr_lo;
+            regs[3] = autoload_c64_adr_hi = byte;
+            break;
+        case 4:
+            regs[4] = autoload_reu_adr_lo = byte;
+            regs[5] = autoload_reu_adr_hi;
+            break;
+        case 5:
+            regs[4] = autoload_reu_adr_lo;
+            regs[5] = autoload_reu_adr_hi = byte;
+            break;
+        case 6:
+            regs[6] = autoload_reu_adr_bank = byte;
+            break;
+        case 7:
+            regs[7] = autoload_length_lo = byte;
+            regs[8] = autoload_length_hi;
+            break;
+        case 8:
+            regs[7] = autoload_length_lo;
+            regs[8] = autoload_length_hi = byte;
+            break;
+        case 11:    // Unconnected registers
+        case 12:
+        case 13:
+        case 14:
+        case 15:
+            break;
+        case 1:     // Command register
+            regs[1] = byte;
+            if ((byte & 0x90) == 0x90) 
+            {
+                execute_dma();
+            }
+            break;
+        default:
+            regs[adr & 0xf] = byte;
+            break;
+    }
+}
+
+
+
+/*
+ *  Execute REU DMA transfer
+ */
+
+void REU::execute_dma()
+{
+    // Clear execute bit in command register
+    regs[1] &= 0x7f;
+
+    // Set FF00 disable bit in command register
+    regs[1] |= 0x10;
+
+    // Get C64 and REU transfer base addresses
+    uint16_t c64_adr = regs[2] | (regs[3] << 8);
+    uint32_t reu_adr = regs[4] | (regs[5] << 8) | (regs[6] << 16);
+
+    // Get transfer length
+    uint16_t length = regs[7] | (regs[8] << 8);
+
+    // Calculate address increments
+    unsigned c64_inc = (regs[10] & 0x80) ? 0 : 1;
+    unsigned reu_inc = (regs[10] & 0x40) ? 0 : 1;
+    
+    if (((regs[1] & 3) == 0) && ((reu_adr & 1) == 0) && ((c64_adr & 1) == 0))
+    {
+        memcpy(&ex_ram[reu_adr & ram_mask], &myRAM[c64_adr], length);
+        regs[0] |= 0x40;    // Transfer finished
+    }
+    else if (((regs[1] & 3) == 1) && ((reu_adr & 1) == 0) && ((c64_adr & 1) == 0))
+    {
+        memcpy(&myRAM[c64_adr], &ex_ram[reu_adr & ram_mask], length);
+        regs[0] |= 0x40;    // Transfer finished
+    }
+    else
+    {    
+        // Do transfer
+        bool verify_error = false;
+        while (!verify_error) 
+        {
+            switch (regs[1] & 3) 
+            {
+                case 0:     // C64 -> REU
+                    ex_ram[reu_adr & ram_mask] = myRAM[c64_adr];
+                    break;
+                case 1:     // C64 <- REU
+                    myRAM[c64_adr] = ex_ram[reu_adr & ram_mask];
+                    break;
+                case 2:     // C64 <-> REU
+                {
+                    uint8_t tmp = myRAM[c64_adr];
+                    myRAM[c64_adr] = ex_ram[reu_adr & ram_mask];
+                    ex_ram[reu_adr & ram_mask] = tmp;
+                    break;
+                }
+                case 3:     // Compare
+                    if (ex_ram[reu_adr & ram_mask] != myRAM[c64_adr]) 
+                    {
+                        regs[0] |= 0x20;    // Verify error
+                        verify_error = true;
+                    }
+                    break;
+            }
+
+            c64_adr += c64_inc;
+            reu_adr += reu_inc;
+            if (--length == 0)
+            {
+                regs[0] |= 0x40;    // Transfer finished
+                break;
+            }
+        }
+    }
+
+    // Update address and length registers
+    if (regs[1] & 0x20)
+    {
+        regs[2] = autoload_c64_adr_lo;
+        regs[3] = autoload_c64_adr_hi;
+        regs[4] = autoload_reu_adr_lo;
+        regs[5] = autoload_reu_adr_hi;
+        regs[6] = autoload_reu_adr_bank;
+        regs[7] = autoload_length_lo;
+        regs[8] = autoload_length_hi;
+    }
+    else 
+    {
+        reu_adr &= ram_mask;
+        regs[2] = c64_adr & 0xff;
+        regs[3] = c64_adr >> 8;
+        regs[4] = reu_adr & 0xff;
+        regs[5] = (reu_adr >> 8) & 0xff;
+        regs[6] = (reu_adr >> 16) & 0xff;
+        regs[7] = length & 0xff;
+        regs[8] = (length >> 8) & 0xff;
+    }
+}
---
+++ arm9/source/REU.h
@@ -0,0 +1,89 @@
+// =====================================================================================
+// GimliDS Copyright (c) 2025 Dave Bernazzani (wavemotion-dave)
+//
+// As GimliDS is a port of the Frodo emulator for the DS/DSi/XL/LL handhelds,
+// any copying or distribution of this emulator, its source code and associated
+// readme files, with or without modification, are permitted per the original 
+// Frodo emulator license shown below.  Hugest thanks to Christian Bauer for his
+// efforts to provide a clean open-source emulation base for the C64.
+//
+// Numerous hacks and 'unsafe' optimizations have been performed on the original 
+// Frodo emulator codebase to get it running on the small handheld system. You 
+// are strongly encouraged to seek out the official Frodo sources if you're at
+// all interested in this emulator code.
+//
+// The GimliDS emulator is offered as-is, without any warranty. Please see readme.md
+// =====================================================================================
+
+/*
+ *  REU.h - 17xx REU and GeoRAM emulation
+ *
+ *  Frodo Copyright (C) Christian Bauer
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef REU_H
+#define REU_H
+
+#include "Cartridge.h"
+
+
+class MOS6510;
+class Prefs;
+
+
+// REU cartridge object
+class REU : public Cartridge {
+public:
+    REU(MOS6510 * cpu);
+    ~REU();
+
+    void Reset() override;
+
+    uint8_t ReadIO2(uint16_t adr, uint8_t bus_byte) override;
+    void WriteIO2(uint16_t adr, uint8_t byte) override;
+
+private:
+    void execute_dma();
+
+    MOS6510 * the_cpu;  // Pointer to 6510 object
+
+    uint8_t * ex_ram;   // Expansion RAM
+
+    uint32_t ram_size;  // Size of expansion RAM
+    uint32_t ram_mask;  // Expansion RAM address bit mask
+
+    uint8_t regs[16];   // REU registers
+
+    uint8_t autoload_c64_adr_lo;    // Autoload registers
+    uint8_t autoload_c64_adr_hi;
+    uint8_t autoload_reu_adr_lo;
+    uint8_t autoload_reu_adr_hi;
+    uint8_t autoload_reu_adr_bank;
+    uint8_t autoload_length_lo;
+    uint8_t autoload_length_hi;
+};
+
+// RAM expansion types
+enum {
+    REU_NONE,       // No REU
+    REU_128K,       // 128K REU
+    REU_256K,       // 256K REU
+    REU_512K,       // 512K REU
+    REU_GEORAM      // 512K GeoRAM
+};
+
+#endif // ndef REU_H
