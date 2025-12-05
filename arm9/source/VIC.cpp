a52f8e88c8c1c8ab4f38d55cd2a9e72990b62783
Dave Bernazzani <meeplemaster@gmail.com>
2025-12-05 08:26:50 -0500

    Version 1.5d with NTSC support - can switch PAL vs NTSC on a per-game basis. New CIA timer tweaks support added. Minor cleanup and optmization.

--- arm9/source/VIC.cpp
+++ arm9/source/VIC.cpp
@@ -1410,7 +1410,7 @@ int MOS6569::EmulateLine(void)
         if (raster >= FIRST_DMA_LINE && raster <= LAST_DMA_LINE && ((raster & 7) == y_scroll) && bad_lines_enabled)
         {
             is_bad_line = true;
-            cycles_left = BAD_CYCLES_PER_LINE + CycleDeltas[myConfig.cpuCycles];
+            cycles_left = (myConfig.tvType ? BAD_CYCLES_PER_LINE_NTSC:BAD_CYCLES_PER_LINE_PAL) + CycleDeltas[myConfig.cpuCycles];
         }
         goto VIC_nop;
     }
@@ -1431,7 +1431,7 @@ int MOS6569::EmulateLine(void)
         {
             // Turn on display
             display_state = is_bad_line = true;
-            cycles_left = BAD_CYCLES_PER_LINE + CycleDeltas[myConfig.cpuCycles];
+            cycles_left = (myConfig.tvType ? BAD_CYCLES_PER_LINE_NTSC:BAD_CYCLES_PER_LINE_PAL) + CycleDeltas[myConfig.cpuCycles];
             rc = 0;
 
             // Read and latch 40 bytes from video matrix and color RAM
